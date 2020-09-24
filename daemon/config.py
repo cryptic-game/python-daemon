91516e77015be876817c835fac5ccc740c0f71fd
Defelo <elodef42@gmail.com>
2020-09-25 00:52:21 +0200

    Fixed env var default

--- daemon/config.py
+++ daemon/config.py
@@ -2,5 +2,5 @@ import os
 
 API_TOKEN = os.getenv("API_TOKEN")
 HOST = os.getenv("HOST", "0.0.0.0")
-PORT = int(os.getenv("PORT", 8000))
+PORT = int(os.getenv("PORT", "8000"))
 DEBUG = os.getenv("DEBUG", "false").lower() in ("true", "t", "yes", "y", "1")
