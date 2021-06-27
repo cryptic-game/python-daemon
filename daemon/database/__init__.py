213f9cf5e5cc6c088d6f1640f459b956255f14d0
Defelo <elodef42@gmail.com>
2021-06-27 09:58:19 +0200

    Improved database

--- daemon/database/__init__.py
+++ daemon/database/__init__.py
@@ -1,3 +1,31 @@
+from contextlib import asynccontextmanager
+from functools import wraps
+
 from database.database import get_database
 
+
+@asynccontextmanager
+async def db_context():
+    """Async context manager for database sessions."""
+
+    db.create_session()
+    try:
+        yield
+    finally:
+        await db.commit()
+        await db.close()
+
+
+def db_wrapper(f):
+    """Decorator which wraps an async function in a database context."""
+
+    @wraps(f)
+    async def inner(*args, **kwargs):
+        async with db_context():
+            return await f(*args, **kwargs)
+
+    return inner
+
+
+# global database connection object
 db = get_database()
--- daemon/database/database.py
+++ daemon/database/database.py
@@ -1,21 +1,74 @@
-from typing import TypeVar
-
-from sqlalchemy import create_engine
+from asyncio import Event
+from contextvars import ContextVar
+from functools import partial
+from typing import TypeVar, Optional, Type
 
 # noinspection PyProtectedMember
-from sqlalchemy.engine import Engine
-from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session, Session, Query
+from sqlalchemy.engine import URL
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, AsyncEngine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.future import select as sa_select, Select
+from sqlalchemy.orm import selectinload
+from sqlalchemy.sql import Executable
+from sqlalchemy.sql.expression import exists as sa_exists, delete as sa_delete, Delete
+from sqlalchemy.sql.functions import count
+from sqlalchemy.sql.selectable import Exists
 
-from config import (
-    SQL_SERVER_LOCATION,
-    SQL_SERVER_DATABASE,
-    SQL_SERVER_USERNAME,
-    SQL_SERVER_PASSWORD,
+from environment import (
+    DB_DRIVER,
+    DB_HOST,
+    DB_PORT,
+    DB_DATABASE,
+    DB_USERNAME,
+    DB_PASSWORD,
     SQL_SHOW_STATEMENTS,
+    POOL_RECYCLE,
+    POOL_SIZE,
+    MAX_OVERFLOW,
 )
+from logger import get_logger
 
 T = TypeVar("T")
+
+logger = get_logger(__name__)
+
+
+def select(entity, *args) -> Select:
+    """Shortcut for :meth:`sqlalchemy.future.select`"""
+
+    if not args:
+        return sa_select(entity)
+
+    options = []
+    for arg in args:
+        if isinstance(arg, (tuple, list)):
+            head, *tail = arg
+            opt = selectinload(head)
+            for x in tail:
+                opt = opt.selectinload(x)
+            options.append(opt)
+        else:
+            options.append(selectinload(arg))
+
+    return sa_select(entity).options(*options)
+
+
+def filter_by(cls, *args, **kwargs) -> Select:
+    """Shortcut for :meth:`sqlalchemy.future.Select.filter_by`"""
+
+    return select(cls, *args).filter_by(**kwargs)
+
+
+def exists(*entities, **kwargs) -> Exists:
+    """Shortcut for :meth:`sqlalchemy.future.select`"""
+
+    return sa_exists(*entities, **kwargs)
+
+
+def delete(table) -> Delete:
+    """Shortcut for :meth:`sqlalchemy.sql.expression.delete`"""
+
+    return sa_delete(table)
 
 
 class DB:
@@ -28,30 +81,58 @@ class DB:
     Base: :class:`sqlalchemy.ext.declarative.DeclarativeMeta`
     """
 
-    def __init__(self, location: str, database: str, username: str, password: str, echo: bool = False):
+    def __init__(
+        self,
+        driver: str,
+        host: str,
+        port: int,
+        database: str,
+        username: str,
+        password: str,
+        pool_recycle: int = 300,
+        pool_size: int = 20,
+        max_overflow: int = 20,
+        echo: bool = False,
+    ):
         """
-        :param location: location of the sql server
+        :param driver: name of the sql connection driver
+        :param host: host of the sql server
+        :param port: port of the sql server
         :param database: name of the database
         :param username: name of the sql user
         :param password: password of the sql user
         :param echo: whether sql queries should be logged
         """
 
-        protocol, location = location.split("://")
-        self.engine: Engine = create_engine(
-            f"{protocol}://{username}:{password}@{location}/{database}",
+        self.engine: AsyncEngine = create_async_engine(
+            URL.create(
+                drivername=driver,
+                username=username,
+                password=password,
+                host=host,
+                port=port,
+                database=database,
+            ),
             pool_pre_ping=True,
-            pool_recycle=300,
-            pool_size=10,
-            max_overflow=20,
+            pool_recycle=pool_recycle,
+            pool_size=pool_size,
+            max_overflow=max_overflow,
             echo=echo,
         )
 
-        self._SessionFactory: sessionmaker = sessionmaker(bind=self.engine)
-        self._Session = scoped_session(self._SessionFactory)
-        self.Base: DeclarativeMeta = declarative_base()
+        self.Base = declarative_base()
 
-    def add(self, obj: T) -> T:
+        self._session: ContextVar[Optional[AsyncSession]] = ContextVar("session", default=None)
+        self._close_event: ContextVar[Optional[Event]] = ContextVar("close_event", default=None)
+
+    async def create_tables(self):
+        """Create all tables defined in enabled cog packages."""
+
+        logger.debug("creating tables")
+        async with self.engine.begin() as conn:
+            await conn.run_sync(partial(self.Base.metadata.create_all))
+
+    async def add(self, obj: T) -> T:
         """
         Add a new row to the database
 
@@ -62,7 +143,7 @@ class DB:
         self.session.add(obj)
         return obj
 
-    def delete(self, obj: T) -> T:
+    async def delete(self, obj: T) -> T:
         """
         Remove a row from the database
 
@@ -70,29 +151,72 @@ class DB:
         :return: the same row
         """
 
-        self.session.delete(obj)
+        await self.session.delete(obj)
         return obj
 
-    def query(self, *entities, **kwargs) -> Query:
-        """Shortcut for :meth:`sqlalchemy.orm.session.Session.query`"""
+    async def exec(self, statement: Executable, *args, **kwargs):
+        """Execute an sql statement and return the result."""
 
-        return self.session.query(*entities, **kwargs)
+        return await self.session.execute(statement, *args, **kwargs)
 
-    def commit(self):
-        """Shortcut for :meth:`sqlalchemy.orm.session.Session.commit`"""
+    async def stream(self, statement: Executable, *args, **kwargs):
+        """Execute an sql statement and stream the result."""
 
-        self.session.commit()
+        return (await self.session.stream(statement, *args, **kwargs)).scalars()
 
-    def close(self):
+    async def all(self, statement: Executable, *args, **kwargs) -> list[T]:
+        """Execute an sql statement and return all results as a list."""
+
+        return [x async for x in await self.stream(statement, *args, **kwargs)]
+
+    async def first(self, *args, **kwargs):
+        """Execute an sql statement and return the first result."""
+
+        return (await self.exec(*args, **kwargs)).scalar()
+
+    async def exists(self, *args, **kwargs):
+        """Execute an sql statement and return whether it returned at least one row."""
+
+        return await self.first(exists(*args, **kwargs).select())
+
+    async def count(self, *args, **kwargs):
+        """Execute an sql statement and return the number of returned rows."""
+
+        return await self.first(select(count()).select_from(*args, **kwargs))
+
+    async def get(self, cls: Type[T], *args, **kwargs) -> Optional[T]:
+        """Shortcut for first(filter_by(...))"""
+
+        return await self.first(filter_by(cls, *args, **kwargs))
+
+    async def commit(self):
+        """Shortcut for :meth:`sqlalchemy.ext.asyncio.AsyncSession.commit`"""
+
+        if self._session.get():
+            await self.session.commit()
+
+    async def close(self):
         """Close the current session"""
 
-        self._Session.remove()
+        if self._session.get():
+            await self.session.close()
+            self._close_event.get().set()
+
+    def create_session(self) -> AsyncSession:
+        """Create a new async session and store it in the context variable."""
+
+        self._session.set(session := AsyncSession(self.engine))
+        self._close_event.set(Event())
+        return session
 
     @property
-    def session(self) -> Session:
-        """Get the session object for the current thread"""
+    def session(self) -> AsyncSession:
+        """Get the session object for the current task"""
 
-        return self._Session()
+        return self._session.get()
+
+    async def wait_for_close_event(self):
+        await self._close_event.get().wait()
 
 
 def get_database() -> DB:
@@ -103,9 +227,14 @@ def get_database() -> DB:
     """
 
     return DB(
-        location=SQL_SERVER_LOCATION,
-        database=SQL_SERVER_DATABASE,
-        username=SQL_SERVER_USERNAME,
-        password=SQL_SERVER_PASSWORD,
+        driver=DB_DRIVER,
+        host=DB_HOST,
+        port=DB_PORT,
+        database=DB_DATABASE,
+        username=DB_USERNAME,
+        password=DB_PASSWORD,
+        pool_recycle=POOL_RECYCLE,
+        pool_size=POOL_SIZE,
+        max_overflow=MAX_OVERFLOW,
         echo=SQL_SHOW_STATEMENTS,
     )
