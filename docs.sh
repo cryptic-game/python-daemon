e0fda5aff6abc77472c65bc9de762db62fa3e747
Defelo <elodef42@gmail.com>
2021-07-05 01:01:16 +0200

    Removed Authorization section from auto generated docs

--- docs.sh
+++ docs.sh
@@ -5,4 +5,4 @@ set -ex
 mkdir -p build
 DEBUG=1 python -c 'from daemon.daemon import app;import json;print(json.dumps(app.openapi()))' | tee build/spec
 docker build -t redoc-cli github.com/Redocly/redoc#:cli
-docker run --rm -v $(pwd)/build:/build redoc-cli bundle /build/spec -o /build/docs.html
+docker run --rm -v $(pwd)/build:/build redoc-cli bundle /build/spec -o /build/docs.html --options.noAutoAuth --options.noAutoAuth
