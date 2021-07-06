68ef2e4ed6e3bdb80d29fd5dcc13b92432fb54f4
Defelo <elodef42@gmail.com>
2021-07-06 18:24:44 +0200

    Added unit test for get endpoint (#25)

--- tests/endpoints/test_counter.py
+++ tests/endpoints/test_counter.py
@@ -1,11 +1,13 @@
 from unittest import IsolatedAsyncioTestCase
+from unittest.mock import patch, MagicMock
 
 from daemon.endpoints import counter
 from daemon.exceptions.counter import CounterNotFoundException, WrongPasswordException
+from daemon.models.counter import Counter
 from daemon.schemas.counter import ValueResponse, ValueChangedResponse
 from daemon.schemas.ok import OKResponse
 from daemon.utils import responses
-from tests._utils import Endpoint, test_endpoint_collection
+from tests._utils import Endpoint, test_endpoint_collection, AsyncMock
 
 ENDPOINTS: list[Endpoint] = [
     Endpoint("exception", "exception", {}),
@@ -23,3 +25,24 @@ class TestCounterEndpoints(IsolatedAsyncioTestCase):
     async def test__exception(self):
         with self.assertRaises(ZeroDivisionError):
             await counter.exception()
+
+    @patch("daemon.endpoints.counter.db")
+    async def test__get__not_found(self, db_patch: MagicMock):
+        user_id = MagicMock()
+        db_patch.get = AsyncMock(return_value=None)
+
+        with self.assertRaises(CounterNotFoundException):
+            await counter.get(user_id)
+
+        db_patch.get.assert_called_once_with(Counter, user_id=user_id)
+
+    @patch("daemon.endpoints.counter.db")
+    async def test__get__successful(self, db_patch: MagicMock):
+        user_id = MagicMock()
+        mock = MagicMock()
+        db_patch.get = AsyncMock(return_value=mock)
+
+        result = await counter.get(user_id)
+
+        db_patch.get.assert_called_once_with(Counter, user_id=user_id)
+        self.assertEqual({"value": mock.value}, result)
