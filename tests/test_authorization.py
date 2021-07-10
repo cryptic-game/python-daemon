32ebe157a8faedd48693a412b0703b1c8ec8ae09
Defelo <elodef42@gmail.com>
2021-07-10 12:13:27 +0200

    Improved authorization tests (#11)

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -2,70 +2,78 @@ from unittest import IsolatedAsyncioTestCase
 from unittest.mock import MagicMock, patch, AsyncMock
 
 from fastapi import HTTPException
-from fastapi.openapi.models import HTTPBearer
 
 from daemon.authorization import HTTPAuthorization
 
 
 class TestAuthorization(IsolatedAsyncioTestCase):
-    @patch("daemon.authorization.API_TOKEN", "TEST_API_TOKEN")
-    async def test__init__(self):
+    @patch("daemon.authorization.API_TOKEN")
+    @patch("daemon.authorization.HTTPBearer")
+    async def test__constructor(self, httpbearer_patch: MagicMock, api_token_patch: MagicMock):
         authorization = HTTPAuthorization()
 
-        self.assertEqual(authorization.scheme_name, authorization.__class__.__name__)
-        self.assertIsInstance(authorization.model, HTTPBearer)
-        self.assertEqual(authorization._token, "TEST_API_TOKEN")
+        httpbearer_patch.assert_called_once_with()
+        self.assertEqual(httpbearer_patch(), authorization.model)
+        self.assertEqual(authorization.__class__.__name__, authorization.scheme_name)
+        self.assertEqual(api_token_patch, authorization._token)
 
-    @patch("daemon.authorization.API_TOKEN", None)
-    async def test___check_authorization__without_token(self):
-        authorization = HTTPAuthorization()
+    async def test__check_authorization__no_token(self):
+        authorization = MagicMock(_token=None)
+
+        result = await HTTPAuthorization._check_authorization(authorization, ...)
+
+        self.assertTrue(result)
+
+    async def test__check_authorization__no_authorization_header(self):
+        authorization = MagicMock()
         request = MagicMock()
+        request.headers.get.return_value = None
 
-        result = await authorization._check_authorization(request)
+        result = await HTTPAuthorization._check_authorization(authorization, request)
 
-        self.assertEqual(result, True)
-
-    @patch("daemon.authorization.API_TOKEN", "TEST_API_TOKEN")
-    async def test___check_authorization__with_token(self):
-        authorization = HTTPAuthorization()
-        request = MagicMock()
-        request.headers.get.return_value = "Bearer TEST_API_TOKEN"
-
-        result = await authorization._check_authorization(request)
-
-        self.assertEqual(result, True)
+        self.assertFalse(result)
         request.headers.get.assert_called_once_with("Authorization")
 
-    @patch("daemon.authorization.API_TOKEN", "TEST_API_TOKEN")
-    async def test___check_authorization__with_wrong_token(self):
-        authorization = HTTPAuthorization()
+    async def test__check_authorization__wrong_token(self):
+        authorization = MagicMock()
         request = MagicMock()
-        request.headers.get.return_value = "Bearer TEST_WRONG_API_TOKEN"
+        request.headers.get().removeprefix.return_value = MagicMock()
+        request.reset_mock()
 
-        result = await authorization._check_authorization(request)
+        result = await HTTPAuthorization._check_authorization(authorization, request)
 
-        self.assertEqual(result, False)
+        self.assertFalse(result)
         request.headers.get.assert_called_once_with("Authorization")
+        request.headers.get().removeprefix.assert_called_once_with("Bearer ")
 
-    @patch("daemon.authorization.API_TOKEN", "TEST_API_TOKEN")
-    @patch("daemon.authorization.HTTPAuthorization._check_authorization")
-    async def test__call(self, check_authorization_patch: AsyncMock):
-        authorization = HTTPAuthorization()
+    async def test__check_authorization__correct_token(self):
+        authorization = MagicMock()
         request = MagicMock()
-        check_authorization_patch.return_value = True
+        request.headers.get().removeprefix.return_value = authorization._token
+        request.reset_mock()
 
-        await authorization.__call__(request)
+        result = await HTTPAuthorization._check_authorization(authorization, request)
 
-        check_authorization_patch.assert_called_once_with(request)
+        self.assertTrue(result)
+        request.headers.get.assert_called_once_with("Authorization")
+        request.headers.get().removeprefix.assert_called_once_with("Bearer ")
 
-    @patch("daemon.authorization.API_TOKEN", "TEST_API_TOKEN")
-    @patch("daemon.authorization.HTTPAuthorization._check_authorization")
-    async def test__call__with_failed_check(self, check_authorization_patch: AsyncMock):
-        authorization = HTTPAuthorization()
+    async def test__call__successful(self):
+        authorization = MagicMock(_check_authorization=AsyncMock())
         request = MagicMock()
-        check_authorization_patch.return_value = False
+        authorization._check_authorization.return_value = True
 
-        with self.assertRaises(HTTPException):
-            await authorization.__call__(request)
+        await HTTPAuthorization.__call__(authorization, request)
 
-        check_authorization_patch.assert_called_once_with(request)
+        authorization._check_authorization.assert_called_once_with(request)
+
+    async def test__call__with_failed_check(self):
+        authorization = MagicMock(_check_authorization=AsyncMock())
+        request = MagicMock()
+        authorization._check_authorization.return_value = False
+
+        with self.assertRaises(HTTPException) as context:
+            await HTTPAuthorization.__call__(authorization, request)
+
+        authorization._check_authorization.assert_called_once_with(request)
+        self.assertEqual(401, context.exception.status_code)
